20d94c66a08a14f75a42950857f5a2eace8bc435
sadia93 <sharminoweshie0035@gmail.com>
2022-03-28 11:32:33 +0600

    initial commit

---
+++ lib/my_themes.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/material.dart';
+
+class AppColors{
+  /*configurable colors stars*/
+  static Color accentColor = Color.fromRGBO(230,46,4, 1);
+  static Color softAccentColor = Color.fromRGBO(247,189,168, 1);
+  static Color splashScreenColor = Color.fromRGBO(	255, 248, 186,1); // if not sure , use the same color as accent color
+  /*configurable colors ends*/
+
+
+  /*If you are not a developer, do not change the bottom colors*/
+  static Color white = Color.fromRGBO(255,255,255, 1);
+  static Color lightGrey = Color.fromRGBO(239,239,239, 1);
+  static Color darkGrey = Color.fromRGBO(112,112,112, 1);
+  static Color mediumGrey = Color.fromRGBO(132,132,132, 1);
+  static Color grey153 = Color.fromRGBO(153,153,153, 1);
+  static Color fontGrey = Color.fromRGBO(73,73,73, 1);
+  static Color textFieldGrey = Color.fromRGBO(209,209,209, 1);
+  static Color golden = Color.fromRGBO(248, 181, 91, 1);
+  static Color shimmerBase = Colors.grey.shade50;
+  static Color shimmerHighlighted = Colors.grey.shade200;
+
+
+
+
+}
